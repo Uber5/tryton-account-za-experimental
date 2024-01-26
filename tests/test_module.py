e53cde672324c721368bf20926a1c7edac7a57bd
Chris Oloff <chris@uber5.com>
2024-01-26 11:55:43 +0200

    initial

---
+++ tests/test_module.py
@@ -0,0 +1,10 @@
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class AccountZaExperimentalTestCase(ModuleTestCase):
+    "Test Account Za Experimental module"
+    module = 'account_za_experimental'
+
+
+del ModuleTestCase
